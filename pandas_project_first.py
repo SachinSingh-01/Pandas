f2648fcfdbf32420ced7fe9bafaf7d55b40b2c77
Sachin Singh <sachinsingh19012@gmail.com>
2026-04-21 20:37:51 +0530

    move to advance

--- pandas_project_first.py
+++ pandas_project_first.py
@@ -10,17 +10,35 @@ Q3.Check:All column names
 Q4.Check:Data types of each column'''
 
 # Question 1
-first10_rows=df.head(10)
-print(first10_rows)
+# first10_rows=df.head(10)
+# print(first10_rows)
 
 # Question  2
-total_anime=len(df["name"])
-print("total anime:",total_anime)
+# total_anime=len(df["name"])
+# print("total anime:",total_anime)
 
 # Question 3
-column_name=df.columns
-print(column_name)
+# column_name=df.columns
+# print(column_name)
 
 # Question 4
-datatype_column=df.dtypes
-print(datatype_column)
+# datatype_column=df.dtypes
+# print(datatype_column)
+
+# Cleaning 
+'''Q5.Find:Missing values in each column
+Q6.Clean:Replace unknown ratings (NaN)
+Q7.Convert:episodes column into numeric
+(Hint: some values are "Unknown")'''
+
+# Question 5
+find_missing_value=df.isna().sum()
+print(find_missing_value)
+
+# Question 6
+df["rating"] = df["rating"].fillna(df["rating"].mean())
+
+# Question 7
+df["episodes"] = df["episodes"].replace("Unknown", pd.NA)
+df["episodes"] = pd.to_numeric(df["episodes"])
+
